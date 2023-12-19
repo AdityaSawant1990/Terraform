dd3bc35fb375c8dcc8afa25702c9e8006e1b498f
Aditya <aditya.sawant30490@gmail.com>
2023-12-19 20:12:18 +0530

    minor changes

--- resource2.tf
+++ resource2.tf
@@ -1,9 +1,9 @@
 #Resource 2 Block
-
-resource "aws_vpc" "TeraVPC1" {
+/*resource "aws_vpc" "TeraVPC1" {
   cidr_block = "10.0.0.0/16"
   provider = aws.aws-us-west-1  #Ths is a Meta Argument
   tags = {
     "Name" = "MyVPC2"
   }
 }
+*/
